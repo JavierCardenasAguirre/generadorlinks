1dada164051e05cfd7b9df624719f6789d054498
JavierCardenasAguirre <javiertrader1979@outlook.com>
2026-08-28 23:37:32 -0500

    colores de cart enlaces

--- next.config.js
+++ next.config.js
@@ -6,7 +6,16 @@ const nextConfig = {
     remotePatterns: [
       { protocol: 'https', hostname: '*.supabase.co' },
       { protocol: 'https', hostname: '*.vercel.app' },
-      { protocol: 'http', hostname: 'localhost' }
+      { protocol: 'http', hostname: 'localhost' },
+      { protocol: 'https', hostname: 'i.imgur.com' },
+      { protocol: 'https', hostname: 'imgur.com' },
+      { protocol: 'https', hostname: 'i.ibb.co' },
+      { protocol: 'https', hostname: 'ibb.co' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'drive.google.com' },
+      { protocol: 'https', hostname: '*.googleusercontent.com' },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+      { protocol: 'https', hostname: 'images.unsplash.com' }
     ]
   }
 }

4226ae7cacc1ddc09de99526a6a5b84a792f130c
JavierCardenasAguirre <javiertrader1979@outlook.com>
2026-09-04 18:53:53 -0500

    update enlaces facebook

--- app/pinkcases/page.js
+++ app/pinkcases/page.js
@@ -62,7 +62,7 @@ export default function PinkCasesPage() {
           <button 
             onClick={() => openLink(
               'fb://profile/100065142186668',
-              'https://www.facebook.com/profile.php?id=100065142186668'
+              'https://www.facebook.com/profile.php?id=100065142186668?sk=about'
             )}
             className="w-full bg-gradient-to-r from-blue-500 to-blue-600 hover:from-blue-600 hover:to-blue-700 text-white font-semibold py-3 px-4 rounded-xl transition-all duration-200 shadow-md hover:shadow-lg flex items-center justify-center gap-2"
           >
